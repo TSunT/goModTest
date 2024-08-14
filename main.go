3de9c5cac31ced1da7f5f90ee4fa0354ae00c68a
0260000309 <0260000309@zwxict.com>
2024-08-14 20:59:59 +0800

    firstCommit

---
+++ main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+func main() {
+	db, err := sql.Open("mysql", "root:root@tcp(192.168.1.21:3306)/glkj-hb")
+	if err != nil {
+		fmt.Println("Error connect to database! cause: ", err)
+		return
+	}
+	defer db.Close()
+	db.SetMaxOpenConns(5)
+	// 测试连接
+	err = db.Ping()
+	if err != nil {
+		fmt.Println("Error connect to database! cause: ", err)
+		return
+	}
+	fmt.Println("Successfully connect to MySQL!")
+	rows, errQuery := db.Query("select id, account, employee_name name from fbp_employee t where t.enable_flag = 1 limit 10")
+	if errQuery != nil {
+		fmt.Println("one error happened when query! cause: ", errQuery)
+		return
+	}
+	defer rows.Close()
+	for rows.Next() {
+		var id, account, name string
+		err = rows.Scan(&id, &account, &name)
+		if err != nil {
+			fmt.Println("error Scanning row: ", err)
+			continue
+		}
+		fmt.Printf("id: %s, name: %s, account: %s\n", id, name, account)
+	}
+	// 检查遍历过程
+	if err = rows.Err(); err != nil {
+		fmt.Println("error iterating through rows: ", err)
+	}
+
+}
